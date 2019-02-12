16657182da07c5f2726b72587d80bb7bc9754a17
Isaac R <isaacr.@MacBook-Pro.local>
2019-02-11 19:28:40 -0800

    First commit

---
+++ src/com/Isaac/ISaveable.java
@@ -0,0 +1,12 @@
+package com.Isaac;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public interface ISaveable {
+
+    List<String> write();
+    void read(List<String> savedValues);
+
+
+}
